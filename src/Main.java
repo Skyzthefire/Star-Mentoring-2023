8efe63f22f02012406f72113bccdb368badb7275
Skyzthefire <130879930+Skyzthefire@users.noreply.github.com>
2023-07-14 20:45:51 +0000

    Kin Val 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final float TRACK_WIDTH_MIN = 4;
+        final float TRACK_WIDTH_MAX = 12;
+        final float VELOCITY_MIN = -100;
+        final float VELOCITY_MAX = 12;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the track width of the robot");
+        System.out.print("between " + TRACK_WIDTH_MIN + "and" + TRACK_WIDTH_MAX + "inches: ")
+        float trackWidth = 0;
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+            try {
+                
+            }
+        }
+    }
+
+
+}
