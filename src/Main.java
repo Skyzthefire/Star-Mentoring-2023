f6bc5f28bad874903b7c976558db48596715ef27
Skyzthefire <130879930+Skyzthefire@users.noreply.github.com>
2023-07-19 01:08:27 +0000

    Fixing non valid inputs

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,38 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter the track width of the robot");
-        System.out.print("between " + TRACK_WIDTH_MIN + "and" + TRACK_WIDTH_MAX + "inches: ")
+        System.out.print("between " + TRACK_WIDTH_MIN + "and" + TRACK_WIDTH_MAX + "inches: ");
         float trackWidth = 0;
 
         while (true) {
             String input = scanner.nextLine().trim();
             try {
-                
+                trackWidth = Float.parseFloat(input);
+            } catch (NumberFormatException ignored) {
+                System.out.println("Input was not a valid floating point number");
+                 System.out.println("Please reenter the input: ");
+                 continue;
             }
+
+            if (trackWidth > TRACK_WIDTH_MAX || trackWidth < TRACK_WIDTH_MIN) {
+                System.out.println("Track width was not between " +
+                TRACK_WIDTH_MIN + "and" + TRACK_WIDTH_MAX + "inches");
+                continue; 
+            }
+            break;
         }
+
+        System.out.print("Please enter the velocity of the Front Left Wheel (Inches per second): ");
+        float frontLeftVelocity = scanner.nextFloat();
+
+        System.out.print("Please enter the velocity of the Front Right Wheel (Inches per second): ");
+        float frontRightVelocity = scanner.nextFloat();
+
+        System.out.print("Please enter the velocity of the Back Left Wheel (Inches per second): ");
+        float backLeftVelocity = scanner.nextFloat();
+
+        System.out.print("Please enter the velocity of the Back Right Wheel (Inches per second): ");
+        float backRightVelocity = scanner.nextFloat();
     }
 
 
